f250757d32bafbf9b0b26e65196976435102c06b
Lucas <lucasmicaoliveira@gmail.com>
2024-10-08 10:07:28 -0300

    primeiro commit na branch

--- Teste.java
+++ Teste.java
@@ -1,6 +1,9 @@
 public class Teste {
     public static void main(String[] args) {
-        System.out.println("Olá mundo!");
-        System.out.println("E olá cidade!");
+        System.out.println("Olá mundo!"); //primeiro commit
+
+        System.out.println("E olá cidade!"); //segundo commit
+
+        System.out.println("Olá Canela"); //primeiro commit na branch
     }
 }
